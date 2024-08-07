0fee98fc702edf511237db144c20603c40a2cf5e
Anderson Palmerim <andersonp.viana.contato@gmail.com>
2024-08-07 14:03:15 -0300

    docs: adding swagger

--- src/main/java/com/anderson/testing_deploy/TestingDeployApplication.java
+++ src/main/java/com/anderson/testing_deploy/TestingDeployApplication.java
@@ -1,9 +1,16 @@
 package com.anderson.testing_deploy;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "testing-deploy",
+		version = "1",
+		description = "project created to test deploy")
+)
 public class TestingDeployApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/anderson/testing_deploy/entrypoint/user/controller/UserController.java
+++ src/main/java/com/anderson/testing_deploy/entrypoint/user/controller/UserController.java
@@ -6,6 +6,9 @@ import com.anderson.testing_deploy.core.user.usecases.port.DeleteUserUseCasePort
 import com.anderson.testing_deploy.core.user.usecases.port.FindUserByIdUseCasePort;
 import com.anderson.testing_deploy.core.user.usecases.port.RegisterUserUseCasePort;
 import com.anderson.testing_deploy.core.user.usecases.port.UpdateUserUseCasePort;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,6 +34,12 @@ public class UserController {
 
 
     @PostMapping("/register")
+    @Operation(summary = "Register a new user", description = "Register a new user with the provided details.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "400", description = "Bad Request: Email already registered"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<User> post(@RequestBody UserDTO obj) {
         var user = registerUser.execute(obj);
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
@@ -41,18 +50,36 @@ public class UserController {
     }
 
     @GetMapping("/get/{id}")
+    @Operation(summary = "Get user by ID", description = "Retrieve a user by their unique ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User found and returned"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<User> get(@PathVariable UUID id) {
         var user = findUserById.execute(id);
         return ResponseEntity.ok(user);
     }
 
     @PutMapping("/put/{id}")
+    @Operation(summary = "Update user details", description = "Update user information by ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User updated successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<User> put(@PathVariable UUID id, @RequestBody UserDTO obj) {
         var user = updateUser.execute(id, obj);
         return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete user by ID", description = "Delete a user by their unique ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         deleteUser.execute(id);
         return ResponseEntity.noContent().build();
