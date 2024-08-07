840bc4689745bf516061282229331a8be7f39464
Anderson Palmerim <andersonp.viana.contato@gmail.com>
2024-08-07 12:50:54 -0300

    feat: creating project

---
+++ src/main/java/com/anderson/testing_deploy/TestingDeployApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.testing_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingDeployApplication.class, args);
+	}
+
+}
