2565297c460c5593abfb1087c3943a5e205c0fc5
Anderson Palmerim <andersonp.viana.contato@gmail.com>
2024-08-07 13:33:40 -0300

    feat: creating configuration layer

---
+++ src/main/java/com/anderson/testing_deploy/config/beans/UserBeans.java
@@ -0,0 +1,45 @@
+package com.anderson.testing_deploy.config.beans;
+
+import com.anderson.testing_deploy.core.user.dataprovider.UserRepository;
+import com.anderson.testing_deploy.core.user.domain.User;
+import com.anderson.testing_deploy.core.user.dtos.UserDTO;
+import com.anderson.testing_deploy.core.user.usecases.impl.DeleteUserUseCaseImpl;
+import com.anderson.testing_deploy.core.user.usecases.impl.FindUserByIdUseCaseImpl;
+import com.anderson.testing_deploy.core.user.usecases.impl.RegisterUserUseCaseImpl;
+import com.anderson.testing_deploy.core.user.usecases.impl.UpdateUserUseCaseImpl;
+import com.anderson.testing_deploy.core.user.usecases.port.DeleteUserUseCasePort;
+import com.anderson.testing_deploy.core.user.usecases.port.FindUserByIdUseCasePort;
+import com.anderson.testing_deploy.core.user.usecases.port.RegisterUserUseCasePort;
+import com.anderson.testing_deploy.core.user.usecases.port.UpdateUserUseCasePort;
+import com.anderson.testing_deploy.dataprovider.user.repositories.impl.UserRepositoryImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserBeans {
+
+    @Bean
+    public UserRepository userRepository() {
+        return new UserRepositoryImpl();
+    }
+
+    @Bean
+    public RegisterUserUseCasePort registerUserUseCasePort(UserRepository repository) {
+        return new RegisterUserUseCaseImpl(repository);
+    }
+
+    @Bean
+    public FindUserByIdUseCasePort findUserByIdUseCasePort(UserRepository repository) {
+        return new FindUserByIdUseCaseImpl(repository);
+    }
+
+    @Bean
+    public UpdateUserUseCasePort updateUserUseCasePort(UserRepository repository, FindUserByIdUseCasePort findUserById) {
+        return new UpdateUserUseCaseImpl(repository, findUserById);
+    }
+
+    @Bean
+    public DeleteUserUseCasePort deleteUserUseCasePort(UserRepository repository, FindUserByIdUseCasePort findUserById) {
+        return new DeleteUserUseCaseImpl(repository, findUserById);
+    }
+}
